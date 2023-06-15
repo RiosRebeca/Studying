9cc299c7c2d8eba8ac4715dff7ef292364e361a5
RiosRebeca <rebeca.rios@ufpe.br>
2023-06-15 15:29:33 -0300

    Letter to my future me

---
+++ Codedex/letter.py
@@ -0,0 +1,7 @@
+print("A little letter to my future me")
+
+# A little letter to my future me
+print(" Now, I'm feeling fine.")
+print("When I commplished this course, i want to find a job.")
+print("please, don´t give up! Piano and programming can walk side by side.")
+print("👀")
