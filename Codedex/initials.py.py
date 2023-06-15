a88fb83327d6e5dbd6514ac5e1220b3262e47935
RiosRebeca <rebeca.rios@ufpe.br>
2023-06-15 10:32:38 -0300

    Mudança no arquivo initials.py

---
+++ Codedex/initials.py.py
@@ -0,0 +1,10 @@
+print("Codedex lesson")
+
+#I am Lactose intolerant 
+print("RRRRRRRRRRRR     KKK        KKK")
+print("RRR      RRRR    KKK     KKKK")
+print("RRR       RRR    KKK    KKKK")
+print("RRRRRRRRRR       KKKKKKKKK")
+print("RRR    RRR       KKK   KKKK")
+print("RRR       RRR    KKK    KKKK")
+print("RRR         RRR  KKK      KKKK")
