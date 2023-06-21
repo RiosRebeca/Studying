762b47cf9fe4edb714288c31f5147719cbcf7c7e
RiosRebeca <rebeca.rios@ufpe.br>
2023-06-21 17:03:45 -0300

    currency

--- Codedex/pythagorean.py
+++ Codedex/pythagorean.py
@@ -5,4 +5,4 @@ b=input("qual o valor de b?")
 
 c = float(int(a)**2 + int(b)**2)**0.5
 
-print(c)
+print(c)           
---
+++ currency.py
@@ -0,0 +1,12 @@
+print("currency")
+
+peso=input("quantos soles você tem?")
+reais=input("quantos resias você tem?")
+soles=input("quantos soles você tem?")
+
+dolar = (float(peso)*0.004) + (float(soles)*0.28) + (float(reais)*0.21)
+
+print("No total você tem"+ str(dolar))
+
+
+
