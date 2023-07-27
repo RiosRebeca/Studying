80ba7dfac27107da23f3fd21a2ee107cf0a716c4
fagner521 <fagner.all@hotmail.com>
2023-07-27 19:52:49 -0300

    Resolução do exercicio.

--- udemy_python/exercicios/secao 7/s7q13.py
+++ udemy_python/exercicios/secao 7/s7q13.py
@@ -3,12 +3,18 @@ print("EXERCÍCIO - SEÇÃO 7")
 # s7q13
 
 '''
-Faça um programa que leia um vetor de 10 posições e verifique se existem valores iguais e os escreva ne tela.
+Faça um programa que leia uma lista de 10 posições e verifique se existem valores iguais e os escreva na tela.
 '''
 
 lista = []
+repetidos = []
 
 for i in range(10):
-    e = input("Adicione um intem: ")
+    e = input("Adicione um item: ")
     lista.append(e)
-    
+
+for item in lista:
+    quantas_vezes_repetiu = lista.count(item)
+    if quantas_vezes_repetiu > 1 and not item in repetidos:
+        repetidos.append(item)
+        print(f"Este {item} repetiu {quantas_vezes_repetiu}")
