a0cfafc42bd17963a75b3388667b3ec400fa0f88
RiosRebeca <rebeca.rios@ufpe.br>
2023-11-14 18:23:52 -0300

    Exercício das pirâmides

--- udemy_python/teste.py
+++ udemy_python/teste.py
@@ -1,32 +1,15 @@
-print("ESPEÇO PARA TESTE")
-
+print("ESPAÇO PARA TESTE")
 
 '''
-Q23 Escreva uma função que gera um triângulo lateral de altura 2*n-1 e n largura. Por exemplo, a saída para n = 4 seria:
+Q24 Escreva uma função que gera um triângulo de altura e lados n e base 2*n-1. Por exemplo, a saída para n = 6 seria:
 '''
 
-
-def triangulo_lateral(n):
+def piramide(n):
     for i in range(n):
-        parte_1 = '*' * i
-        print(parte_1)
-    for j in range(n, 0, - 1):
-        parte_2 = '*' * j 
-        print(parte_2)
-
-print(triangulo_lateral(4))
+        altura = '@' * i
+        altura = ' ' * n
+        print(altura)
 
 
-'''
-como formar um triângiulo?
-
-
-1- 1 + n até chegar na LINHA CENTRAL
-2- A LINHA CENTRAL É A MAIS LONGA
-3- LINHA CENTRAL - 1 até chegar a 1
-
-
-
-for vai pegar o número do andar no range, se for primeiro andar ele vai fazer andar = '*' * numero do andar 
-
-'''
+print(piramide(10))
+        
